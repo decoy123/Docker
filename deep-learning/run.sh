3086a3395044c0f945cc5b6cbf4f7670b32ed474
Unknown <develop123456789@gmail.com>
2017-07-26 18:28:37 +0900

    コンテナのタイムゾーンをホストと同期するように変更

--- deep-learning/run.sh
+++ deep-learning/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nvidia-docker run -d --rm -p 8022:22 --name deep-learning -v /data/workspace:/data/workspace:rw -v /data/workspace/Docker/deep-learning/.pycharm_helpers:/root/.pycharm_helpers:rw decoy/deep-learning
+nvidia-docker run -d --rm -p 8022:22 --name deep-learning -v /etc/localtime:/etc/localtime:ro -v /data/workspace:/data/workspace:rw -v /data/workspace/Docker/deep-learning/.pycharm_helpers:/root/.pycharm_helpers:rw decoy/deep-learning
